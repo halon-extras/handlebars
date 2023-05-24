1244e3f0b78ce8e4db64637f28830aab951b5181
Fredrik Lengstrand <fredrik.lengstrand@halon.se>
2023-05-24 10:01:27 +0000

    Use helper functions

--- handlebars.go
+++ handlebars.go
@@ -7,12 +7,70 @@ package main
 import "C"
 import (
 	"encoding/json"
+	"errors"
+	"fmt"
 	"unsafe"
 
 	"github.com/aymerick/raymond"
 )
 
 func main() {}
+
+func GetArgumentAsString(args *C.HalonHSLArguments, pos uint64, required bool) (string, error) {
+	var x = C.HalonMTA_hsl_argument_get(args, C.ulong(pos))
+	if x == nil {
+		if required {
+			return "", fmt.Errorf("missing argument at position %d", pos)
+		} else {
+			return "", nil
+		}
+	}
+	var y *C.char
+	if C.HalonMTA_hsl_value_get(x, C.HALONMTA_HSL_TYPE_STRING, unsafe.Pointer(&y), nil) {
+		return C.GoString(y), nil
+	} else {
+		return "", fmt.Errorf("invalid argument at position %d", pos)
+	}
+}
+
+func GetArgumentAsJSON(args *C.HalonHSLArguments, pos uint64, required bool) (string, error) {
+	var x = C.HalonMTA_hsl_argument_get(args, C.ulong(pos))
+	if x == nil {
+		if required {
+			return "", fmt.Errorf("missing argument at position %d", pos)
+		} else {
+			return "", nil
+		}
+	}
+	var y *C.char
+	z := C.HalonMTA_hsl_value_to_json(x, &y, nil)
+	defer C.free(unsafe.Pointer(y))
+	if z {
+		return C.GoString(y), nil
+	} else {
+		return "", fmt.Errorf("invalid argument at position %d", pos)
+	}
+}
+
+func SetReturnValueToAny(ret *C.HalonHSLValue, val interface{}) error {
+	x, err := json.Marshal(val)
+	if err != nil {
+		return err
+	}
+	y := C.CString(string(x))
+	defer C.free(unsafe.Pointer(y))
+	var z *C.char
+	if !(C.HalonMTA_hsl_value_from_json(ret, y, &z, nil)) {
+		if z != nil {
+			err = errors.New(C.GoString(z))
+			C.free(unsafe.Pointer(z))
+		} else {
+			err = errors.New("failed to parse return value")
+		}
+		return err
+	}
+	return nil
+}
 
 //export Halon_version
 func Halon_version() C.int {
@@ -24,64 +82,33 @@ func Halon_init(hic *C.HalonInitContext) C.bool {
 	return true
 }
 
-func set_ret_value(ret *C.HalonHSLValue, key string, value string) {
-	var ret_key *C.HalonHSLValue
-	var ret_value *C.HalonHSLValue
-	C.HalonMTA_hsl_value_array_add(ret, &ret_key, &ret_value)
-	key_cs := C.CString(key)
-	key_cs_up := unsafe.Pointer(key_cs)
-	defer C.free(key_cs_up)
-	value_cs := C.CString(value)
-	value_cs_up := unsafe.Pointer(value_cs)
-	defer C.free(value_cs_up)
-
-	C.HalonMTA_hsl_value_set(ret_key, C.HALONMTA_HSL_TYPE_STRING, key_cs_up, 0)
-	C.HalonMTA_hsl_value_set(ret_value, C.HALONMTA_HSL_TYPE_STRING, value_cs_up, 0)
-}
-
 //export handlebars
 func handlebars(hhc *C.HalonHSLContext, args *C.HalonHSLArguments, ret *C.HalonHSLValue) {
-	var template string
-	var handlebars_cs *C.char
-
-	var args_0 = C.HalonMTA_hsl_argument_get(args, 0)
-	if args_0 != nil {
-		if !C.HalonMTA_hsl_value_get(args_0, C.HALONMTA_HSL_TYPE_STRING, unsafe.Pointer(&handlebars_cs), nil) {
-			set_ret_value(ret, "error", "Invalid type of \"template\" argument")
-			return
-		}
-		template = C.GoString(handlebars_cs)
-	} else {
-		set_ret_value(ret, "error", "Missing required \"id\" argument")
-		return
-	}
-
-	var context_str string
-	var args_1 = C.HalonMTA_hsl_argument_get(args, 1)
-	if args_1 != nil {
-		var context_cs *C.char
-		var context_size_t C.size_t
-		var success = C.HalonMTA_hsl_value_to_json(args_1, &context_cs, &context_size_t)
-		context_str = C.GoString(context_cs)
-		C.free(unsafe.Pointer(context_cs))
-		if !success {
-			set_ret_value(ret, "error", context_str)
-			return
-		}
-	} else {
-		set_ret_value(ret, "error", "Missing required \"context\" argument")
-		return
-	}
-
-	var context map[string]interface{}
-	json.Unmarshal([]byte(context_str), &context)
-
-	result, err := raymond.Render(template, context)
+	template, err := GetArgumentAsString(args, 0, true)
 	if err != nil {
-		set_ret_value(ret, "error", err.Error())
+		value := map[string]interface{}{"error": err.Error()}
+		SetReturnValueToAny(ret, value)
+		return
+	}
+	context, err := GetArgumentAsJSON(args, 1, true)
+	if err != nil {
+		value := map[string]interface{}{"error": err.Error()}
+		SetReturnValueToAny(ret, value)
+		return
 	}
 
-	set_ret_value(ret, "result", result)
+	var ctx map[string]interface{}
+	json.Unmarshal([]byte(context), &ctx)
+
+	result, err := raymond.Render(template, ctx)
+	if err != nil {
+		value := map[string]interface{}{"error": err.Error()}
+		SetReturnValueToAny(ret, value)
+		return
+	}
+
+	value := map[string]interface{}{"result": result}
+	SetReturnValueToAny(ret, value)
 }
 
 //export Halon_hsl_register
